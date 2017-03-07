4d39dbb2470f0beb30af40022ec385a037df2118
sekhar karri <babusek@gmail.com>
2017-03-07 23:37:14 +0000

    Adding Happy Discount calculator implementation

--- src/main/java/uk/co/buygroceries/impl/HappySalesDiscountCalculatorImpl.java
+++ src/main/java/uk/co/buygroceries/impl/HappySalesDiscountCalculatorImpl.java
@@ -1,11 +1,37 @@
 package uk.co.buygroceries.impl;
 
+import uk.co.buygroceries.base.ItemEnum;
 import uk.co.buygroceries.interfaces.Cart;
 import uk.co.buygroceries.interfaces.DiscountCalculator;
+import uk.co.buygroceries.model.Item;
 
 public class HappySalesDiscountCalculatorImpl implements DiscountCalculator {
 
+  private static final String BREAD = "BREAD";
+
   public Double calculateDiscount(Cart cart) {
+    Double discount = 0D;
+    for (Item item : cart.listItems()) {
+      switch (ItemEnum.valueOf(item.getName())) {
+        case Milk:
+          discount += item.getItemDiscount();
+          break;
+        case Butter:
+          discount += (item.getQuantity() / 2) * getBreadDiscountPrice(cart);
+          break;
+        default:
+          break;
+      }
+    }
+    return discount;
+  }
+
+  private Double getBreadDiscountPrice(Cart cart) {
+    for (Item item : cart.listItems()) {
+      if (BREAD.equalsIgnoreCase(item.getName())) {
+        return item.getItemDiscount();
+      }
+    }
     return 0D;
   }
 
