bbfce7d45421ba32439010d38b08d420e683477d
sekhar karri <babusek@gmail.com>
2017-03-07 23:10:01 +0000

    Adding a discount implementor called Happy sales implementor

---
+++ src/main/java/uk/co/buygroceries/impl/HappySalesDiscountCalculatorImpl.java
@@ -0,0 +1,13 @@
+package uk.co.buygroceries.impl;
+
+import uk.co.buygroceries.interfaces.Cart;
+import uk.co.buygroceries.interfaces.DiscountCalculator;
+
+public class HappySalesDiscountCalculatorImpl implements DiscountCalculator {
+
+  public Double calculateDiscount(Cart cart) {
+    return 0D;
+  }
+
+
+}
