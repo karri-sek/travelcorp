b30b1a6e9a5a27c254c806fe054c2fbef4055684
sekhar karri <babusek@gmail.com>
2017-03-07 22:42:12 +0000

    Adding implementation of add items to the cart to pass the add items test

--- src/main/java/uk/co/buygroceries/impl/ShoppingCartImpl.java
+++ src/main/java/uk/co/buygroceries/impl/ShoppingCartImpl.java
@@ -1,5 +1,6 @@
 package uk.co.buygroceries.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import uk.co.buygroceries.interfaces.Cart;
@@ -7,10 +8,12 @@ import uk.co.buygroceries.model.Item;
 
 public class ShoppingCartImpl implements Cart {
 
+  private List<Item> shoppingCartProducts = new ArrayList<Item>();
+
 
 
   public void addItem(Item product) {
-  
+    shoppingCartProducts.add(product);
   }
 
   public void deleteItem(Item product) {
@@ -18,7 +21,7 @@ public class ShoppingCartImpl implements Cart {
   }
 
   public List<Item> listItems() {
-    return null;
+    return shoppingCartProducts;
   }
 
 }
